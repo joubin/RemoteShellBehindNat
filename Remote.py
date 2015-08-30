44f6be720af13d557379601f01a8fc4d27904fa0
Joubin Jabbari <joubin.j@gmail.com>
2015-08-30 14:21:38 -0700

    Adding directed messages
    adding the ability to target one computer

--- Remote.py
+++ Remote.py
@@ -8,35 +8,41 @@ HOST='irc.freenode.net'  # You can use any service however, my code has only bee
 PORT=6667 
 NICK='BigData'+str(random.randint(1, 1000)) # You can name it whatever you want. 
 #BigData + somenumber if you are going to have more than one machine
-IDENT=NICK
-REALNAME=NICK 
-OWNER='user' # Change this to your username 
+REALNAME = IDENT = "Something"+NICK 
+ 
+OWNER='ChangeOwner' # Change this to your username 
 
-CHANNELINIT='#channel' #Rename this to something that people wont join to. To limit noise by other people 
+CHANNELINIT='#ChangeChannel' #Rename this to something that people wont join to. To limit noise by other people 
 readbuffer='' 
 
 def parsemsg(msg): 
+    wasDirected = 0
     complete=msg[1:].split(':',1)
     info=complete[0].split(' ') 
     msgpart=complete[1] 
     sender=info[0].split('!') 
     if sender[0]==OWNER: 
         cmd=msgpart
-        test = os.popen(cmd).read()
-        if test != '':
-            test = test.split()
-            s.send( 'PRIVMSG '+CHANNELINIT+' :'+str(test)+'\r\n' );
+    if cmd != '':
+        cmd = cmd.split()
+        if '@' in cmd[0]:
+        wasDirected = 1
+        if cmd[0].replace('@','') not in NICK:
+            return
+    cmd = ' '.join(cmd[wasDirected:])
+        commandOutput = os.popen(cmd).read()
+        if commandOutput != '':
+            commandOutput = commandOutput.split()
+            s.send( 'PRIVMSG '+CHANNELINIT+' :'+str(commandOutput)+'\r\n' );
 
 
 s=socket.socket( ) 
 s.connect((HOST, PORT)) 
 s.send('NICK '+NICK+'\r\n')  
-
 s.send('USER '+IDENT+' '+HOST+' bla :'+REALNAME+'\r\n') 
-while 1: 
 
+while True: 
     line=s.recv(200)
-    print line 
     if line.find('Welcome ')!=-1: 
         s.send('JOIN '+CHANNELINIT+'\r\n') 
     if line.find('joubin')!=-1 and line.find('JOIN')==-1: 
@@ -44,6 +50,6 @@ while 1:
         parsemsg(line)
         
     if(line.find('PING')!=-1):
-		pongData = line.split(':', 1)
-		pongData = pongData[1].split(' ')
-		s.send('PONG '+pongData[0]+'\r\n')
+        pongData = line.split(':', 1)
+        pongData = pongData[1].split(' ')
+        s.send('PONG '+pongData[0]+'\r\n')
